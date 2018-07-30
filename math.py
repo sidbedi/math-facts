d7a2ac39a0a2a6a0392ba6515f900a8b83355cf1
Sid Bedi <sidbedi@yahoo.com>
2018-07-30 15:46:28 -0500

    made math questions dynamic

--- math.py
+++ math.py
@@ -1,22 +1,20 @@
+import random
+
+
 def ask_problem():
 
      global score
-
-
-     guess = input('what is 2+2? ')
-
+     operand1 = random.randint(1,12)
+     operand2 = random.randint(1,12)
+     guess = input('what is ' + str(operand1) + ' + ' + str(operand2) + '? ')
      #change this line to define answer
      #hint answer = 
-     answer = '4'
-
+     answer = operand1 + operand2
+     guess=int(guess)
      if guess == answer:
-
         print('correct answer')
-
         score = score + 1
-
      if guess != answer:
-
         print('wrong answer!')
 
 
@@ -24,11 +22,13 @@ def ask_problem():
 
 
 score = 0
-
+runme=1
 print('welcome to math quiz!')
+while runme < 10:
+     ask_problem()
+     runme=runme+1
 
-ask_problem()
-
-print ('good game!!! :)')
+print('your score is ' + str(score) + "\n")
+print('good game!!! :)')
 
 
